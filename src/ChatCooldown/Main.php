12ca670365558a8224464c75b421a10e1562bcfe
applqpak <applqpak@gmail.com>
2016-04-03 19:52:00 +1000

    Update Main.php

--- src/ChatCooldown/Main.php
+++ src/ChatCooldown/Main.php
@@ -11,6 +11,8 @@
   class Main extends PluginBase implements Listener
   {
 
+    private $cooldown = array();
+
     public function dataPath()
     {
 
@@ -30,12 +32,44 @@
 
       @mkdir($this->dataPath());
 
-      $this->cfg = new Config($this->dataPath() . "config.yml", Config::YAML, array("cooldown" => 5));
+      $this->cfg = new Config($this->dataPath() . "config.yml", Config::YAML, array("cooldown" => 5, "cooldown_message" => "Woah, Do not chat so quickly {name}! You may chat in {time} more second(s)."));
 
     }
 
     public function onChat(PlayerChatEvent $event)
     {
+
+      $player = $event->getPlayer();
+
+      $player_name = $player->getName();
+
+      $cooldown = $this->cfg->get("cooldown");
+
+      $cooldown_message = $this->cfg->get("cooldown_message");
+
+      if(isset($this->cooldown[$player_name]))
+      {
+
+        $last_chat_time = $this->cooldown[$player_name];
+
+        if((time() - $last_chat_time) <= $cooldown)
+        {
+
+          $timeDifference = (time() - $last_chat_time);
+
+          $player->sendMessage(str_replace(array("{name}", "{time}"), array($player_name, $timeDifference)));
+
+          $event->setCancelled();
+
+        }
+
+      }
+      else
+      {
+
+        $this->cooldown[$player_name] = time();
+
+      }
 
     }
 
